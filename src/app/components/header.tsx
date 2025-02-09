07f3866237bfaf4fd0e6837ca11bbc37329f99d6
mraza24 <161523925+mraza24@users.noreply.github.com>
2025-02-09 07:46:43 +0500

    Updated package.json, page.tsx, and added components

---
+++ src/app/components/header.tsx
@@ -0,0 +1,10 @@
+// src/components/header.tsx
+const Header = () => {
+    return (
+      <header>
+        <h1>Welcome to My Website</h1>
+      </header>
+    );
+  };
+  
+  export default Header;
